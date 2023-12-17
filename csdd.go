55f2e6d9cccbb13da5c25855b716e2262e9aa2b6
Nikita <62812039+sosiskus@users.noreply.github.com>
2023-12-17 22:50:07 +0200

    added gitignore

--- csdd.go
+++ csdd.go
@@ -58,11 +58,11 @@ func scrape() string {
 	}
 
 	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
-	req.Header.Set("Accept-Language", "en-US,en;q=0.9,lv;q=0.8,ru;q=0.7")
+	req.Header.Set("Accept-Language", "en-US,en;q=0.9")
 	req.Header.Set("Cache-Control", "max-age=0")
 	req.Header.Set("Connection", "keep-alive")
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Set("Cookie", "_ga=GA1.1.432155588.1702485824; _ga_KSGMLEJL82=GS1.1.1702494283.3.0.1702494283.0.0.0; PHPSESSID=lheraup71fdttk94nptii9aeu3; eSign=3a2f1edbfd3fc2513016674cb77c89a4; _hjSessionUser_3007240=eyJpZCI6IjNjODE2YTMzLTU5ZDktNWU5YS1iY2QyLTQ5ODU0YzE5OTYxMyIsImNyZWF0ZWQiOjE3MDI3NjM5OTEzMjYsImV4aXN0aW5nIjp0cnVlfQ==; _hjDonePolls=852170; _hjMinimizedPolls=852170; userSawThatSiteUsesCookies=1; _hjIncludedInSessionSample_3007240=0; _hjSession_3007240=eyJpZCI6ImYwNWZlN2U4LTY4OWUtNDgyOS05ZWQwLTg5NmYwN2YwZTY4YSIsImMiOjE3MDI4MjQ3MDAwMDMsInMiOjAsInIiOjAsInNiIjowfQ==; _hjAbsoluteSessionInProgress=0; SimpleSAML=f4dd95a33a50dfbd0c76f0efa5b59a27; SERVERID=s4; SimpleSAMLAuthToken=_fdcd18f9f7c7b287206e721f10e490f883e5b6f6a8; _ga_Q09H2GL8G8=GS1.1.1702824681.5.1.1702825534.0.0.0")
+	req.Header.Set("Cookie", "PHPSESSID=gsl7rrh7suuqt35e3c3ne408ip; eSign=7cf6bd2046f334665e572e9504bfe40c; SERVERID=s6; _hjFirstSeen=1; _hjIncludedInSessionSample_3007240=0; _hjSession_3007240=eyJpZCI6IjUzYTAyZGMwLTk3MDAtNGJjZS05YmU2LTUyZjUwYTcwZjVkZSIsImMiOjE3MDI4NDU5MTU0MzgsInMiOjAsInIiOjAsInNiIjowfQ==; _hjAbsoluteSessionInProgress=0; SimpleSAML=837545cf14ad6ad527dee4b0b26fab1c; SimpleSAMLAuthToken=_06d3a64f432545c8e5517dc278a9f2ce73eadfb396; _ga=GA1.1.2074492875.1702845949; _hjSessionUser_3007240=eyJpZCI6IjU0ODhmZTJlLTliMDYtNWMxMC1iNzY1LWUzODZmZWE1ZTNhZCIsImNyZWF0ZWQiOjE3MDI4NDU5MTU0MzYsImV4aXN0aW5nIjp0cnVlfQ==; _ga_Q09H2GL8G8=GS1.1.1702845949.1.1.1702845992.0.0.0; _hjMinimizedPolls=852170")
 	req.Header.Set("Origin", "https://e.csdd.lv")
 	req.Header.Set("Referer", "https://e.csdd.lv/examp/")
 	req.Header.Set("Sec-Fetch-Dest", "document")
